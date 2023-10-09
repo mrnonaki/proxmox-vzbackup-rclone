6956c81c488d69bd4fa080e63f91f5ee8f9e9bd5
Dmitrii Mukhin <mukhin.dmitrij@gmail.com>
2023-10-09 10:48:56 +0300

    Squashed commit of the following:
    commit ab249d25d4c56117fbfe143948aa8c7d3fd3465e
    Author: Dmitrii Mukhin <88187872+DmitriiMukhin@users.noreply.github.com>
    Date:   Wed Oct 4 11:55:17 2023 +0300
    
        Update vzbackup-rclone.sh

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -2,16 +2,16 @@
 # ./vzbackup-rclone.sh rehydrate YYYY/MM/DD file_name_encrypted.bin
 
 ############ /START CONFIG
-drive="gd-backup_crypt"
-dumpdir="/mnt/pve/pvebackups01/dump" # Set this to where your vzdump files are stored
+DRIVE_NAME="drive_backup"
+DUMP_DIR="/mnt/pve/wd/dump" # Set this to where your vzdump files are stored
 MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
 MAX_CLOUD_AGE=31 # This is the age in days to keep cloud backup copies. Cloud backups older than this are deleted
 ############ /END CONFIG
 
-_bdir="$dumpdir"
-rcloneroot="$dumpdir/rclone"
-timepath="$(date +%Y-%m-%d)"
-rclonedir="$rcloneroot/$timepath"
+_bdir="$DUMP_DIR"
+RCLONE_ROOT="$DUMP_DIR/rclone"
+TIME_PATH="$(date +%Y-%m-%d)"
+RCLONE_DIR="$RCLONE_ROOT/$TIME_PATH"
 COMMAND=${1}
 rehydrate=${2} #enter the date you want to rehydrate in the following format: YYYY/MM/DD
 if [ ! -z "${3}" ];then
@@ -27,24 +27,24 @@ filename=${tarfile%.*.*}
 
 if [[ ${COMMAND} == 'rehydrate' ]]; then
     #echo "Please enter the date you want to rehydrate in the following format: YYYY/MM/DD"
-    #echo "For example, today would be: $timepath"
+    #echo "For example, today would be: $TIME_PATH"
     #read -p 'Rehydrate Date => ' rehydrate
     rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M copy $drive:/$rehydrate$CMDARCHIVE $dumpdir \
+    --drive-chunk-size=32M copy $DRIVE_NAME:/$rehydrate$CMDARCHIVE $DUMP_DIR \
     -v --stats=60s --transfers=16 --checkers=16
 fi
 
 if [[ ${COMMAND} == 'job-start' ]]; then
     echo "Deleting backups older than $MAX_AGE days."
-    find $dumpdir -type f -mtime +$MAX_AGE -exec /bin/rm -f {} \;
+    find $DUMP_DIR -type f -mtime +$MAX_AGE -exec /bin/rm -f {} \;
 fi
 
 if [[ ${COMMAND} == 'backup-end' ]]; then
     echo "Backing up $tarfile to remote storage"
-    echo "rcloning $rclonedir"
+    echo "rcloning $RCLONE_DIR"
     
     rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M copy $tarfile $drive:/$timepath \
+    --drive-chunk-size=32M copy $tarfile $DRIVE_NAME:/$TIME_PATH \
     -v --stats=60s --transfers=16 --checkers=16
 fi
 
@@ -80,7 +80,7 @@ if [[ ${COMMAND} == 'job-end' ||  ${COMMAND} == 'job-abort' ]]; then
     echo "rcloning $_filename4"
 
     rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M move $_filename4 $drive:/$timepath \
+    --drive-chunk-size=32M move $_filename4 $DRIVE_NAME:/$TIME_PATH \
     -v --stats=60s --transfers=16 --checkers=16
 
     rclone --config /root/.config/rclone/rclone.conf \
